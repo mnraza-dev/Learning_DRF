2f2597a4739d47d9f3e45e7e24730c7644de94f9
Md Noorullah Raza <mnraza94@gmail.com>
2025-02-19 21:18:03 +0530

    Add post_student view and URL routing for handling student data submissions

--- basics/django_rest/core/home/views.py
+++ basics/django_rest/core/home/views.py
@@ -11,3 +11,9 @@ def home(request):
     serializer = StudentSerializer(student_data, many=True)
 
     return  Response({'status':200, 'payload': serializer.data})
+
+@api_view(['POST'])
+def post_student(request):
+    data = request.data
+    print(data)
+    return Response({'status':200, 'payload': data, 'message': 'success'})
