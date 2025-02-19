7162d8c3245811b38aa0d9951529127c977d321c
Md Noorullah Raza <mnraza94@gmail.com>
2025-02-19 18:17:10 +0530

    Add Student serializer and myapp data retrieval function

---
+++ basics/gs3/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Student
+
+class StudentSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    roll = serializers.IntegerField()
+    city = serializers.CharField(max_length=100)
+
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+    
