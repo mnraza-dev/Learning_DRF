29741eb10e6ad2e26b10436451ebbcb5bc675c7d
Md Noorullah Raza <mnraza94@gmail.com>
2025-02-19 18:46:26 +0530

    Refactor student API view to handle GET requests with query parameters and improve error handling

--- basics/gs3/api/views.py
+++ basics/gs3/api/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
-import io
+from django.http import JsonResponse
 from .models import Student
 from rest_framework.parsers import JSONParser
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse, JsonResponse
 
 def student_api(request):
     if request.method == 'GET':
-        json_data = request.body    
-        stream = io.BytesIO(json_data)
-        pythondata = JSONParser().parse(stream)
-        id = pythondata.get('id', None)
+        id = request.GET.get('id', None)
         if id is not None:
-            stu = Student.objects.get(id=id)
-            serializer = StudentSerializer(stu)
-            json_data = JSONRenderer().render(serializer.data)
-            return HttpResponse(json_data, content_type='application/json')
+            try:
+                stu = Student.objects.get(id=id)
+                serializer = StudentSerializer(stu)
+                return JsonResponse(serializer.data, safe=False)
+            except Student.DoesNotExist:
+                return JsonResponse({'error': 'Student not found'}, status=404)
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        return JsonResponse(serializer.data, safe=False)
--- basics/gs3/myapp.py
+++ basics/gs3/myapp.py
@@ -1,14 +1,21 @@
 import requests
 import json
 
-URL ="http://127.0.0.1:8000/studentapi/"
-def get_data(id = None):
- data = {}
- if id is not None:
-    data = {'id':id}
-    json_data = json.dumps(data)
-    r = requests.get(url=URL, data=json_data)
-    data = r.json()
-    print(data) 
+URL = "http://127.0.0.1:8000/studentapi/"
 
-get_data(2)
+def get_data(id=None):
+    params = {}
+    if id is not None:
+        params = {'id': id}
+    
+    r = requests.get(url=URL, params=params)
+    
+    if r.status_code == 200:
+        data = r.json()  
+        print(data)
+    else:
+        print(f"Failed to get data. Status code: {r.status_code}")
+
+
+get_data() 
+# get_data(1) 
