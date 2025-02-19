9cb34959c2f1de9123b43c0245b2f427b138a481
Md Noorullah Raza <mnraza94@gmail.com>
2025-02-19 20:42:55 +0530

    Add home app configuration and update settings for Django project

--- basics/django_rest/core/home/views.py
+++ basics/django_rest/core/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-from rest_framework import Response
+from rest_framework.response import Response
 
 @api_view
 def home(request):
