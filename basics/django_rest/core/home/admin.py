3c22737b07701e12afc7262dcc341ad950e32671
Md Noorullah Raza <mnraza94@gmail.com>
2025-02-20 23:12:18 +0530

    Remove unnecessary whitespace in admin.py for Student model registration

--- basics/django_rest/core/home/admin.py
+++ basics/django_rest/core/home/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Student
-
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['name', 'age', 'email', 'phone', 'city']
