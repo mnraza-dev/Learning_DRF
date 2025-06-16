b44794828e666c4245b5d2f4c601143cf8a61db1
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-16 11:02:29 +0530

    added Serializers

--- drf-project/api/views.py
+++ drf-project/api/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from students.models import Student
+from rest_framework.decorators import api_view
 
+@api_view(['GET'])
 def studentsView(request):
-    return HttpResponse("Hello, World!")
+    if request.method == 'GET':
+        # Get all data
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status= status.HTTP_200_OK )
--- drf-project/students/models.py
+++ drf-project/students/models.py
@@ -1,2 +1,21 @@
 from django.db import models
 
+class Student(models.Model):
+    student_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    email = models.EmailField(max_length=255)
+    phone = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255, choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')])
+    branch = models.CharField(max_length=255)
+    course = models.CharField(max_length=255)
+    year = models.IntegerField()
+    semester = models.IntegerField()
+    cgpa = models.FloatField()
+    is_active = models.BooleanField(default=True)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
